b2c565c7f02045ad28d912501da1240a1dbb0d5c
ShieldedAsh <101831569+ShieldedAsh@users.noreply.github.com>
2022-11-05 13:34:25 -0400

    added newtons first law of physics or something

--- kinematics.gd
+++ kinematics.gd
@@ -1,21 +1,22 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+const GRAVITY = 150.0
+const WALK_SPEED = 200
 
 var velocity = Vector2()
 
-func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed('right'):
-		velocity.x += 1
-	if Input.is_action_pressed('left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('down'):
-		velocity.y += 1
-	if Input.is_action_pressed('up'):
-		velocity.y -= 1
-	velocity = velocity.normalized() * speed
-
 func _physics_process(delta):
-	get_input()
-	move_and_slide(velocity)
+	velocity.y += delta * GRAVITY
+
+	if Input.is_action_pressed("left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("right"):
+		velocity.x =  WALK_SPEED
+	else:
+		velocity.x = 0
+
+	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
+
+	# The second parameter of "move_and_slide" is the normal pointing up.
+	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
+	move_and_slide(velocity, Vector2(0, -1))
