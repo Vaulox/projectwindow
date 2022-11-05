53e7a94b7eb6af5d0e482fc96bb9e0973f889f6a
zappitus <114707227+zappitus@users.noreply.github.com>
2022-11-05 16:54:43 -0400

    temp commit to save what little ive gotten done lol

---
+++ health.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+signal damaged(by)
+signal killed()
+
+
+var health_max = 10
+
+var health = 10
+
+func take_damage(impact):
+	impact = clamp(impact, 0.0, 1.0)
+	var damage = health_max * impact
+	var prev_health = health
+	health -= damage
+	health = clamp(health, 0, health_max)
+
+	if prev_health != health:
+		emit_signal("damaged", damage)
+
+	if health <= 0.0:
+		emit_signal("killed")
+	
