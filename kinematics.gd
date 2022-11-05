21ef98f35fc3618f813b0283fc947b01983458d9
ShieldedAsh <101831569+ShieldedAsh@users.noreply.github.com>
2022-11-05 14:37:43 -0400

    added gravity and the ability to ascend temp.

--- kinematics.gd
+++ kinematics.gd
@@ -1,22 +1,25 @@
 extends KinematicBody2D
 
-const GRAVITY = 150.0
-const WALK_SPEED = 200
+var run_speed = 100
+var jump_speed = -250
+var gravity = 300
 
 var velocity = Vector2()
 
+func get_input():
+	velocity.x = 0
+	var right = Input.is_action_pressed('right')
+	var left = Input.is_action_pressed('left')
+	var jump = Input.is_action_just_pressed('up')
+
+	if is_on_floor() and jump:
+		velocity.y = jump_speed
+	if right:
+		velocity.x += run_speed
+	if left:
+		velocity.x -= run_speed
+
 func _physics_process(delta):
-	velocity.y += delta * GRAVITY
-
-	if Input.is_action_pressed("left"):
-		velocity.x = -WALK_SPEED
-	elif Input.is_action_pressed("right"):
-		velocity.x =  WALK_SPEED
-	else:
-		velocity.x = 0
-
-	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
-
-	# The second parameter of "move_and_slide" is the normal pointing up.
-	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
-	move_and_slide(velocity, Vector2(0, -1))
+	velocity.y += gravity * delta
+	get_input()
+	velocity = move_and_slide(velocity, Vector2(0, -1))
